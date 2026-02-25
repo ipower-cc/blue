3e046bbc9d3a0877d0ad91a36534159fad7a4d72
thinknot <nobody@nothome.org>
2026-02-25 01:07:12 +0000

    hanging comments tests

--- tests/good_cases/hanging_comments.py
+++ tests/good_cases/hanging_comments.py
@@ -4,12 +4,12 @@ c = 333  # Are fine
 
 nested_arr = [
     [1, 4, 7],      # These
-    [10, 13, 16],   # Should be
+    [10, 3, 16],    # Should be
     [19, 22, 25],   # Too
 ]
 
 nested_dict = {
-    1: 2,           # fine
+    1: 2,           # fine in a line
     9000: 100500,   # just in
     3: 4,           # case
 }
